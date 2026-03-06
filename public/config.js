34393baaa0907db2b858d11ef86b5bfaca26695d
A-Nandi99 <236006251+A-Nandi99@users.noreply.github.com>
2026-03-06 19:29:24 +0530

    Update API key and backend URL in config.js

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzhhMTUzYWFhMzU0YmRhNmMxZjZkYWE0NTI0YjU1NCIsIm5iZiI6MTc3MjgwMzA4MC42ODQsInN1YiI6IjY5YWFkNDA4ZGYyYTljYzRlMzg3ODVhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lFP2Hf6xLW6Z9OYgxJrHwnB-CmDcGqAa3f9lrjjZTq4",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -13,7 +13,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: true,
 
   // The backend URL(s) to communicate with - can be a single URL or comma-separated list (e.g., "https://server1.com,https://server2.com")
-  VITE_BACKEND_URL: null,
+  VITE_BACKEND_URL: "https://backend-production-ba864.up.railway.app",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: "",
