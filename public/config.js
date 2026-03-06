82aa33514b55f0c5d713a1bb2366b1386b3a7702
A-Nandi99 <236006251+A-Nandi99@users.noreply.github.com>
2026-03-06 19:52:21 +0530

    Update CORS proxy URL in config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "coruscating-stardust-d64926.netlify.app",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzhhMTUzYWFhMzU0YmRhNmMxZjZkYWE0NTI0YjU1NCIsIm5iZiI6MTc3MjgwMzA4MC42ODQsInN1YiI6IjY5YWFkNDA4ZGYyYTljYzRlMzg3ODVhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lFP2Hf6xLW6Z9OYgxJrHwnB-CmDcGqAa3f9lrjjZTq4",
